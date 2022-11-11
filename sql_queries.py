eb612fb5379b1bd6e24273db4c4057dfd7413784
Bruno Mendes <b96mendes@gmail.com>
2022-11-10 21:17:17 -0300

    Add initial sql queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,44 +1,95 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id SERIAL UNIQUE,
+        start_time TIME,
+        usert_id INTEGER,
+        level TEXT,
+        song_id TEXT,
+        artist_id TEXT,
+        session_id INTEGER,
+        location TEXT,
+        user_agent TEXT
+    )
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        first_name TEXT,
+        last_name TEXT,
+        gender TEXT,
+        level TEXT
+    )
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id TEXT PRIMARY KEY,
+        title TEXT,
+        artist_id TEXT,
+        year FLOAT,
+        duration FLOAT
+    )
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id TEXT PRIMARY KEY,
+        name TEXT,
+        location TEXT,
+        latitude FLOAT,
+        longitude FLOAT
+    )
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time TIME PRIMARY KEY,
+        hour INTEGER,
+        day INTEGER,
+        week INTEGER,
+        month INTEGER,
+        year INTEGER,
+        weekday INTEGER
+    )
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+    INSERT INTO songplays (songplay_id, start_time, usert_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
 user_table_insert = ("""
+    INSERT INTO users (user_id, first_name, last_name, gender, level)
+    VALUES (%s, %s, %s, %s, %s)
 """)
 
 song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
 """)
 
 
 time_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
